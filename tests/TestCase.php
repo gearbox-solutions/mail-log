d5c2d51e198efbca4982a840f22044182f32e779
Michael Deck <michael@gearboxgo.com>
2025-07-03 16:08:42 -0400

    Add files to override use as overrides in Laravel v10

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,6 +4,7 @@ namespace GearboxSolutions\MailLog\Tests;
 
 use GearboxSolutions\MailLog\Providers\MailLogServiceProvider;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Str;
 use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
@@ -31,5 +32,14 @@ class TestCase extends Orchestra
             'database' => ':memory:',
             'prefix' => '',
         ]);
+    }
+
+    protected function resolveApplicationConsoleKernel($app)
+    {
+        if(Str::startsWith($app->version(), '10.')) {
+            $app->singleton(
+                \Illuminate\Contracts\Console\Kernel::class, 'Workbench\App\Console\Kernel'
+            );
+        }
     }
 }
---
+++ workbench/app/Console/Kernel-v10.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Workbench\App\Console;
+
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use function Orchestra\Testbench\workbench_path;
+
+class Kernel extends ConsoleKernel
+{
+    /**
+     * Define the application's command schedule.
+     */
+    protected function schedule(Schedule $schedule): void
+    {
+        $schedule->command('mail-log:build')->hourly();
+    }
+
+    /**
+     * Register the commands for the application.
+     */
+    protected function commands(): void
+    {
+        $this->load(__DIR__.'/Commands');
+
+        require workbench_path('routes/console.php');
+    }
+}
